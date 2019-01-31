2cfc7558316823d238998a42ad425e2adfdcdf0b
marat <gumerovm74@gmail.com>
2019-01-31 23:26:10 +0300

    3 задачи + 3 теста. (+проверка на ноль) Point класс

---
+++ src/main/java/Point/Point.java
@@ -0,0 +1,20 @@
+package Point;
+
+/**
+ * @author Gumerov Marat
+ */
+
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    }
+}
---
+++ src/main/java/Point/PointMain.java
@@ -0,0 +1,12 @@
+package Point;
+
+public class PointMain {
+    public static void main(String[] args) {
+        Point a = new Point(0,1);
+        Point b = new Point(2,5);
+
+        double result = a.distanceTo(b);
+        System.out.println("Расстояние между точками А и В : " + result);
+
+    }
+}
