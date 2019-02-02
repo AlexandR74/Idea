010b3eccdfaf171a1fc36b9b687b286588454637
marat <gumerovm74@gmail.com>
2019-02-02 22:42:13 +0300

    Point выполнен + тест выполнен

--- src/main/java/point/PointMain.java
+++ src/main/java/point/PointMain.java
@@ -2,8 +2,8 @@ package point;
 
 public class PointMain {
     public static void main(String[] args) {
-        Point a = new Point(0,1);
-        Point b = new Point(2,5);
+        Point a = new Point(-1,1);
+        Point b = new Point(-2,5);
 
         double result = a.distanceTo(b);
         System.out.println("Расстояние между точками А и В : " + result);
---
+++ src/test/java/point/PointTest.java
@@ -0,0 +1,40 @@
+package point;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void distanceTo() {
+
+        Point a = new Point(0,1);
+        Point b = new Point(2,5);
+
+        double result = a.distanceTo(b);
+        assertThat(result, closeTo(4.4,0.1));
+    }
+
+
+    @Test
+    public void distanceTo2() {
+
+        Point a = new Point(-1,1);
+        Point b = new Point(-2,5);
+
+        double result = a.distanceTo(b);
+        assertThat(result, closeTo(4.1,0.1));
+    }
+
+    @Test
+    public void distanceTo3() {
+
+        Point a = new Point(0,0);
+        Point b = new Point(0,0);
+
+        double result = a.distanceTo(b);
+        assertThat(result, closeTo(0,0.1));
+    }
+}
