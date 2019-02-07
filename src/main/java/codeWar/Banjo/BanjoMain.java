66b4d485156493bc4dff6616329dde994319a8bf
marat <gumerovm74@gmail.com>
2019-02-07 21:41:01 +0300

    навел порядок в именах пакетов

--- src/main/java/codeWar/Banjo/BanjoMain.java
+++ src/main/java/codeWar/Banjo/BanjoMain.java
@@ -1,13 +1,27 @@
+/**
+ * task from the codewars project
+ */
+
+
 package codeWar.Banjo;
 
 
+/**
+ * The banjo class is determined by the initial
+ * letters in the name of whether you play
+ * the banjo (if the name starts with R r then you play)
+ */
+
+
 public class BanjoMain {
+
+
     public static void main(String[] args) {
 
         Banjo name  = new Banjo();
 
         String a = name.areYouPlayingBanjo("Runner");
-
+       
         System.out.println(a);
-            }
+    }
 }
