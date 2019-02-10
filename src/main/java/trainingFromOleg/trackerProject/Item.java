f3648a30a0c5d8a5fc3743a7d4e0c1c3bea84eaf
marat <gumerovm74@gmail.com>
2019-02-11 01:00:27 +0300

    начало реализации Tracker

--- src/main/java/trainingFromOleg/trackerProject/Item.java
+++ src/main/java/trainingFromOleg/trackerProject/Item.java
@@ -1,4 +1,35 @@
 package trainingFromOleg.trackerProject;
 
 public class Item {
+
+    /**
+     * реализуем поля ( с Астах)
+     */
+
+    /**
+     * инициализируем Id
+     */
+    String id;
+
+    /**
+     * инициализируем name
+     */
+    String name;
+
+    /**
+     * инициализируем desc
+     */
+    String desc;
+
+    /**
+     * инициализируем created
+     */
+    long created;
+
+    /**
+     * инициализируем comments
+     */
+    String [] comments;
+
+
 }
