3bdba27f45284a927a063594cd833c1e9bb4c197
oaryukov <olegaryukov@gmail.com>
2019-02-11 22:15:47 +0300

    fix

--- src/main/java/trainingFromOleg/trackerProject/Item.java
+++ src/main/java/trainingFromOleg/trackerProject/Item.java
@@ -29,18 +29,51 @@ public class Item {
     /**
      * инициализируем comments
      */
-    String [] comments;
+    String[] comments;
 
 
-
-
-
-    public Item(String name, String desc){
+    public Item(String name, String desc) {
         this.name = name;
         this.desc = desc;
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public long getCreated() {
+        return created;
+    }
+
+    public void setCreated(long created) {
+        this.created = created;
+    }
+
+    public String[] getComments() {
+        return comments;
+    }
+
+    public void setComments(String[] comments) {
+        this.comments = comments;
+    }
 }
