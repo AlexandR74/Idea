52ca371f3240586d839f4d0e50fd4690b5c1579f
marat <gumerovm74@gmail.com>
2019-02-25 20:00:33 +0300

    методы написаны // тесты : add/replace/delete/ (остальные затрудняюсь пока).

--- src/test/java/trainingFromOleg/trackerProject/TrackerTest.java
+++ src/test/java/trainingFromOleg/trackerProject/TrackerTest.java
@@ -9,6 +9,9 @@ public class TrackerTest {
 
 
     @Test
+    /**
+     * тест должен проверять добавление заявки
+     */
     public void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
         Item item = new Item("test1","testDescription","123");
@@ -17,6 +20,9 @@ public class TrackerTest {
     }
 
     @Test
+    /**
+     * Тест для метода replace (редактирование заявки  )
+     */
     public void whenReplaceNameThenReturnNewName() {
         Tracker tracker = new Tracker();
         Item previous = new Item("test1","testDescription","123");
@@ -31,5 +37,25 @@ public class TrackerTest {
 // Проверяем, что заявка с таким id имеет новые имя test2.
         assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
     }
+
+    @Test
+    /**
+     * тест должен проверять добавление заявки
+     */
+    public void deletemetodTest() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1","testDescription","123");
+        Item item2 = new Item("test2","testDescription","1234");
+        Item item3 = new Item("test3","testDescription","12356");
+
+        tracker.add(item);
+        tracker.add(item2);
+        tracker.add(item3);
+
+        tracker.delete("1234");
+        tracker.delete("12356");
+        assertThat(tracker.findById("123"), is(item));
+    }
+
 
 }
